37f1edddc58e192487716bb7cf409bd890aa65af
rpokidin <rpokidin@yandex.ru>
2022-06-08 22:11:10 +0300

    fun randomInt and strLengthMax 2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 
 // Cлучайное целое число
-let randomInt = (numMin, numMax) => {
-	if (numMin < 0 || numMax < 0 || numMax <= numMin) {
-		console.log('Задано число в отрицательном диапозоне или max значение меньше или равно min!');
-	} else {
-		numMin = Math.ceil(numMin);
-		numMax = Math.floor(numMax);
-		return Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
-	}
-}
-console.log(randomInt(5, 10));
+const randomInt = (numMin, numMax) => {
+  if (numMin < 0 || numMax < 0 || numMax <= numMin) {
+    return false;
+  } else {
+    numMin = Math.ceil(numMin);
+    numMax = Math.floor(numMax);
+    return Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
+  }
+};
+randomInt(5, 2);
 
 // Максимальная длина строки
-let strLengthMax = (str, strMax) => {
-	if (str.length <= strMax) {
-		return true;
-	} else {
-		return false;
-	}
-}
-console.log(strLengthMax('Текст', 10));
+const strLengthMax = (str, strMax) => {
+  if (str.length <= strMax) {
+    return true;
+  } else {
+    return false;
+  }
+};
+strLengthMax('Текст', 10);
