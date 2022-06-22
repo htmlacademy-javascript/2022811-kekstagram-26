b9232ab5ef7f401b4df38e4b4411e05b67ad4845
rpokidin <rpokidin@yandex.ru>
2022-06-22 23:56:35 +0300

    module5-task1

---
+++ js/mock-names.js
@@ -0,0 +1,4 @@
+export const mockNamesExport = () => {
+  const mockNames = ['Матвей', 'Полина', 'Артём', 'Эмма', 'Есения', 'Денис', 'Андрей', 'Мирон', 'Владислав', 'Семён'];
+  return mockNames;
+};
